920afd5cae1f1b14fe871f18d03fdad325dd674b
Cieciak <nikodemrzasa@wp.pl>
2025-02-12 11:17:36 +0100

    Add BF runner to examples

--- examples/bf.py
+++ examples/bf.py
@@ -33,10 +33,41 @@ loop = sequenceOf(
 prog = many(inst, strict=True)
 
 
+class AST_runner:
+
+    def __init__(self, mem_size: int = 30_000):
+        self.memory = [0, ] * mem_size
+        self.pointer = 0
+
+    def run(self, tree: Status):
+        for element in tree:
+            if isinstance(element, dict):
+                key, *_ = element.keys()
+                val, *_ = element.values()
+            else:
+                key = element
+
+            match key:
+
+                case '+':
+                    self.memory[self.pointer] = (self.memory[self.pointer] + val) % 256
+                case '-':
+                    self.memory[self.pointer] = (self.memory[self.pointer] - val) % 256
+                case '>':
+                    self.pointer = (self.pointer + val) % len(self.memory)
+                case '<':
+                    self.pointer = (self.pointer - val) % len(self.memory)
+                case 'loop':
+                    while self.memory[self.pointer] != 0:
+                        self.run(val)
+                case '.':
+                    print(chr(self.memory[self.pointer]), end='')
+                case ',':
+                    self.memory[self.pointer] = ord(input()[0])
 
 if __name__ == '__main__':
-    s = Status('++++[>+++[>++<-]<-]')
+    s = Status('+++++++++++[[>>]++++++++++[>++++++++++<-]+[<<]>>-]>>>-.>>+++++.>>+.>>-.>>+++++.>>---.>>+++++++.>>[-]<+++++[>++++++++<-]>--.>>.>>+.>>++++++++++++++++++.>++++++++++.')
 
     r = prog.run(s)
-
-    print(r)
+    runner = AST_runner()
+    runner.run(r.result)
