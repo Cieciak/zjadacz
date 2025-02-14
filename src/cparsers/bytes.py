0a8a30c9953725f8cde141f899119962ac2be01b
Cieciak <nikodemrzasa@wp.pl>
2025-02-14 13:39:06 +0100

    Add uint parsers for bytes

--- src/cparsers/bytes.py
+++ src/cparsers/bytes.py
@@ -33,3 +33,37 @@ def qword() -> Parser:
         if len(content) < 8: return ParserError('Not enough data')
         return status.chainResult(content[0:8], increment=2)
     return Parser(check)
+
+def uint8(dir: str = None) -> Parser:
+    return byte().map(lambda s: __uint_from_bytes([s.result, ], 1, True))
+
+def uint16(dir: str) -> Parser:
+    match dir.lower():
+        case 'msb': flag = True
+        case 'lsb': flag = False
+        case _: ValueError('dir must be \'msb\' or \'lsb\'')
+    return word().map(lambda s: __uint_from_bytes(s.result, 2, flag))
+
+def uint32(dir: str) -> Parser:
+    match dir.lower():
+        case 'msb': flag = True
+        case 'lsb': flag = False
+        case _: ValueError('dir must be \'msb\' or \'lsb\'')
+    return dword().map(lambda s: __uint_from_bytes(s.result, 4, flag))
+
+def uint64(dir: str) -> Parser:
+    match dir.lower():
+        case 'msb': flag = True
+        case 'lsb': flag = False
+        case _: ValueError('dir must be \'msb\' or \'lsb\'')
+    return qword().map(lambda s: __uint_from_bytes(s.result, 8, flag))
+
+def __uint_from_bytes(array: bytes, length: int, msb: bool):
+    dire = 1 if msb else -1 
+    data = array[0:length]
+
+    value = 0
+    for b in data[::dire]:
+        value = 256 * value + int(b)
+
+    return value
